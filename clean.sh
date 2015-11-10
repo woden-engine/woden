7454443a7aeb2af16cafa3a303ccb3529b11d411
Ronie Salgado <roniesalg@gmail.com>
2015-11-10 19:55:51 -0300

    I keep working more on these scripts.

--- clean.sh
+++ clean.sh
@@ -3,5 +3,5 @@ MY_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $MY_DIR/scripts/common.sh
 
 cd "$WODEN2_SRC_TOP"
-rm -rf vm build build-dist image
+rm -rf vm build build-dist image play-cache github-cache package-cache github-*.zip
 
