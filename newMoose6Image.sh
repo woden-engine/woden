19a3cfd4dfc2269fdbdbe9526bd7e08698e107b2
Ronie Salgado <roniesalg@gmail.com>
2015-11-10 20:10:51 -0300

    I added a script for loading a new moose image with Woden 2.

--- newMoose6Image.sh
+++ newMoose6Image.sh
@@ -8,9 +8,11 @@ bash ./buildDependencies.sh || exit 1
 IMAGE_DIR="$WODEN2_SRC_TOP"
 cd "$IMAGE_DIR"
 
-# Fetch a new image and a new vm
-wget -O- get.pharo.org/50 | bash
+# Download the Moose 6 image
+wget -c https://ci.inria.fr/moose/job/moose-6.0/lastSuccessfulBuild/artifact/moose-6.0.zip || exit 1
+unzip moose-6.0.zip || exit 1
+rm -f moose-6.0.zip
 
 # Load the repositories into the image
-./woden-vm Pharo.image st "$WODEN2_SRC_TOP/scripts/LoadRepositories.st"
+./woden-vm moose-6.0.image st "$WODEN2_SRC_TOP/scripts/LoadRepositories.st"
 
