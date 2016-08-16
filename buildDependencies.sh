1b9a5b491416208aec692fb3408ad14c7c030a3a
Ronie Salgado <roniesalg@gmail.com>
2016-08-16 03:58:57 +0200

    Newer version of the dependencies. I am starting to add the physics engine.

--- buildDependencies.sh
+++ buildDependencies.sh
@@ -4,11 +4,9 @@ source $MY_DIR/scripts/common.sh
 
 bash "$WODEN2_SRC_TOP/scripts/fetchVM.sh" || exit 1
 
-export WODEN2_BUILD_DIST_DIR="$WODEN2_SRC_TOP/native-libraries"
-export WODEN2_BUILD_DIR="$WODEN2_SRC_TOP/build"
-
 mkdir -p "$WODEN2_BUILD_DIST_DIR"
 mkdir -p "$WODEN2_BUILD_DIR"
 
 bash scripts/build-agpu.sh || exit 1
+bash scripts/build-aphy.sh || exit 1
 
--- scripts/common.sh
+++ scripts/common.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 export WODEN2_SRC_TOP="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/.. && pwd )"
 
+export WODEN2_BUILD_DIST_DIR="$WODEN2_SRC_TOP/native-libraries"
+export WODEN2_BUILD_DIR="$WODEN2_SRC_TOP/build"
