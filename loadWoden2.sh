974bca3587b5df2414a71632f7dbe2a84d4251cb
Ronie Salgado <roniesalg@gmail.com>
2017-02-23 18:32:56 -0300

    Activate the LowcodeFFI on the 64 bits VMs.

--- loadWoden2.sh
+++ loadWoden2.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 if test `uname` = "Darwin"; then
-    # On MacOS X, Metal works only on 64 bits mode
-    bash ./loadWoden64.sh
+    # On MacOS X, Metal works only on 64 bits mode. Avoid using the 64 bits VM, because
+    # it is not completely stable yet.
+    bash ./loadWoden64.sh $@
 else
-    bash ./loadWoden32.sh
+    bash ./loadWoden32.sh $@
 fi
