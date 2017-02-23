28f54bb64bf5be071c8db545685301c389413131
Ronie Salgado <roniesalg@gmail.com>
2017-02-23 03:50:13 -0300

    Oops.

--- fetchImage64.sh
+++ fetchImage64.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 MY_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-source $MY_DIR/scripts/common.sh
+source $MY_DIR/scripts/common64.sh
 
 # Ensure the image folder is built.
 IMAGE_DIR="$WODEN2_SRC_TOP"
