9b83fdd9b622d1c0a96b32c14fdaee12621f94bf
Ronie Salgado <roniesalg@gmail.com>
2017-02-23 16:17:51 -0300

    Download the Pharo sources when needed.

--- fetchVM32.sh
+++ fetchVM32.sh
@@ -5,12 +5,13 @@ source $MY_DIR/scripts/common32.sh
 OS=`uname`
 case $OS in
     Linux)
-        fetch_targz "http://ronie.cl/files/lowcode/linux/release-phcoglowcodelinuxht.tar.gz" release-phcoglowcodelinuxht.tar.gz || exit
+        fetch_targz "http://ronie.cl/files/lowcode/linux/release-phcoglowcodelinuxht.tar.gz" release-phcoglowcodelinuxht.tar.gz || exit 1
         useVM "$WODEN2_SRC_TOP/phcoglowcodelinuxht/pharo"
         ;;
 
     Darwin)
-        fetch_zip "http://ronie.cl/files/lowcode/osx/32x86-pharo.cog.spur.lowcode-Release.zip" 32x86-pharo.cog.spur.lowcode-Release.zip || exit
+        fetch_zip "http://ronie.cl/files/lowcode/osx/32x86-pharo.cog.spur.lowcode-Release.zip" 32x86-pharo.cog.spur.lowcode-Release.zip || exit 1
+        fetch_zip "http://files.pharo.org/get-files/50/sources.zip" PharoSourcesV50.zip || exit 1
         useVM "$WODEN2_SRC_TOP/Pharo.app/Contents/MacOS/Pharo"
         ;;
 
--- fetchVM64.sh
+++ fetchVM64.sh
@@ -5,15 +5,16 @@ source $MY_DIR/scripts/common64.sh
 OS=`uname`
 case $OS in
     Linux)
-        fetch_targz "http://ronie.cl/files/lowcode/linux/release-phcoglowcode64linuxht.tar.gz" release-phcoglowcode64linuxht.tar.gz || exit
+        fetch_targz "http://ronie.cl/files/lowcode/linux/release-phcoglowcode64linuxht.tar.gz" release-phcoglowcode64linuxht.tar.gz || exit 1
         useVM "$WODEN2_SRC_TOP/phcoglowcodelinuxht/pharo"
         ;;
 
     Darwin)
-        fetch_zip "http://ronie.cl/files/lowcode/osx/64x64-pharo.cog.spur.lowcode-Release.zip" 64x64-pharo.cog.spur.lowcode-Release.zip || exit
+        fetch_zip "http://ronie.cl/files/lowcode/osx/64x64-pharo.cog.spur.lowcode-Release.zip" 64x64-pharo.cog.spur.lowcode-Release.zip || exit 1
+        fetch_zip "http://files.pharo.org/get-files/50/sources.zip" PharoSourcesV50.zip || exit 1
         useVM "$WODEN2_SRC_TOP/Pharo.app/Contents/MacOS/Pharo"
         ;;
-        
+
     *)
         echo "Unsupported operating system $OS"
         exit 1
