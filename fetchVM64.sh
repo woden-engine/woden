7ef9787c04afdf5a45aaf68a1613a6afe9e31a0a
Ronie Salgado <roniesalg@gmail.com>
2017-02-23 20:00:54 -0300

    Another fixup for OS X.

--- fetchVM64.sh
+++ fetchVM64.sh
@@ -13,15 +13,9 @@ case $OS in
         fetch_zip "http://ronie.cl/files/lowcode/osx/64x64-pharo.cog.spur.lowcode-Release.zip" 64x64-pharo.cog.spur.lowcode-Release.zip || exit 1
         fetch_zip "http://files.pharo.org/get-files/50/sources.zip" PharoSourcesV50.zip || exit 1
 
-        # Remove the unused libSDL2.dylib
-        rm -f "$WODEN2_SRC_TOP/Pharo.app/Contents/MacOS/Plugins/libSDL2.dylib"
+        # Zip does not preserve a symlink.
+        ln -sf "$WODEN2_SRC_TOP/Pharo.app/Contents/MacOS/Plugins/libSDL2-2.0.0.dylib" "$WODEN2_SRC_TOP/Pharo.app/Contents/MacOS/Plugins/libSDL2.dylib"
 
-        # If we have a system installed version of libSDL2-2.0.0.dylib,
-        # then remove the bundled version. We remove them because they tend to conflict.
-        if test -e "/usr/local/lib/libSDL2-2.0.0.dylib"; then
-            rm -f "$WODEN2_SRC_TOP/Pharo.app/Contents/MacOS/Plugins/libSDL2-2.0.0.dylib"
-        fi
-        
         useVM "$WODEN2_SRC_TOP/Pharo.app/Contents/MacOS/Pharo"
         ;;
 
