d56e817783c5f340a36b476f0c63b79182e0d828
Ronie Salgado <roniesalg@gmail.com>
2017-07-11 17:38:26 -0400

    Improving the cleanup script.

--- clean.sh
+++ clean.sh
@@ -3,4 +3,6 @@ MY_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $MY_DIR/scripts/common_scripts.sh
 
 cd "$WODEN2_SRC_TOP"
-rm -rf vm build build-dist pharo-vm native-libraries play-cache github-cache package-cache github-*.zip
+rm -rf vm build build-dist pharo-vm native-libraries native-libraries-64 build-64 \
+    play-cache github-cache package-cache github-*.zip \
+    abstract-gpu woden2.sh Pharo*
