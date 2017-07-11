f47633b26f8437a6e78dda4649128fbe758279f3
Ronie Salgado <roniesalg@gmail.com>
2017-07-11 18:10:43 -0400

    Oops

--- loadWoden2Image64.sh
+++ loadWoden2Image64.sh
@@ -5,9 +5,8 @@ source $MY_DIR/scripts/common32.sh
 SOURCE_IMAGE_NAME="$1"
 
 if test "$SOURCE_IMAGE_NAME" = ""; then
-    SOURCE_IMAGE_NAME="Pharo64.image"
+    SOURCE_IMAGE_NAME="Pharo.image"
 fi
 
 execVM $SOURCE_IMAGE_NAME st $WODEN2_SRC_TOP/scripts/LoadRepositories.st || exit 1
 echo "Done loading image $SOURCE_IMAGE_NAME with Woden2"
-
