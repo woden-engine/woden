ec5a904535a1c6341aa3f2e5db1e8d812163c095
Ronie Salgado <roniesalg@gmail.com>
2017-07-11 18:06:24 -0400

    Removing more debris.

--- clean.sh
+++ clean.sh
@@ -5,4 +5,6 @@ source $MY_DIR/scripts/common_scripts.sh
 cd "$WODEN2_SRC_TOP"
 rm -rf vm build build-dist pharo-vm native-libraries native-libraries-64 build-64 \
     play-cache github-cache package-cache github-*.zip \
-    abstract-gpu woden2.sh Pharo*
+    abstract-gpu abstract-physics core-assets pharo-local \
+    __MACOSX woden2.sh Pharo* woden2.changes woden2.image \
+    cog_*.tar
