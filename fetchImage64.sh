1ae2d3e7790fbf12d62fd2c3e812f216b246a158
Ronie Salgado <roniesalg@gmail.com>
2017-07-11 18:03:14 -0400

    User the zeroconf script for fetching the 64 bits image.

--- fetchImage64.sh
+++ fetchImage64.sh
@@ -7,12 +7,4 @@ IMAGE_DIR="$WODEN2_SRC_TOP"
 cd "$IMAGE_DIR"
 
 # Fetch a new image
-wget -O Pharo64.zip http://files.pharo.org/image/60/latest-64.zip || exit 1
-
-# Decompress it
-unzip Pharo64.zip
-
-RAW_IMAGE_NAME=`ls Pharo64*.image`
-BASE_NAME=`echo $RAW_IMAGE_NAME | sed 's/\\.image//g'`
-mv "$BASE_NAME.image" "Pharo64.image"
-mv "$BASE_NAME.changes" "Pharo64.changes"
+wget -O- get.pharo.org/64/60 | bash
