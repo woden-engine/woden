73f680297562dc498d249dbcb5795c40576aa830
Ronie Salgado <roniesalg@gmail.com>
2017-12-07 11:02:25 -0300

    Do not build the Phanide library on OS X.

--- scripts/build-phanide.sh
+++ scripts/build-phanide.sh
@@ -2,6 +2,10 @@
 source "$WODEN2_SRC_TOP/scripts/common_scripts.sh"
 fetch_git_dependency "https://github.com/ronsaldo/phanide.git" phanide
 
+if test `uname` == "Darwin"; then
+    exit
+fi
+
 # Create the Phanide build dir.
 BUILD_DIR="$WODEN2_BUILD_DIR/phanide"
 mkdir -p "$BUILD_DIR"
