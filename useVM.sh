4ed91b49cfe4141afc419fa8e3ad259bf85d3b36
Ronie Salgado <roniesalg@gmail.com>
2018-03-19 10:19:19 -0300

    Update the script for selecting a VM.

--- useVM.sh
+++ useVM.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-WODEN2_SRC_TOP="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+WODEN_SRC_TOP="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 VM_NAME="$1"
 if test ! -e "$VM_NAME"; then
@@ -7,18 +7,18 @@ if test ! -e "$VM_NAME"; then
     exit 1
 fi
 
-cat > "$WODEN2_SRC_TOP/woden2.sh" <<EOF
+cat > "$WODEN_SRC_TOP/woden.sh" <<EOF
 #!/bin/sh
 TOP="\$( cd "\$( dirname "$0" )" && pwd )"
 
 OS="\$(uname)"
 if test "\$OS" = "Darwin"; then
-    export DYLD_LIBRARY_PATH="\$TOP/native-libraries-32:\$TOP/native-libraries-64:\$DYLD_LIBRARY_PATH"
+    export DYLD_LIBRARY_PATH="\$TOP:\$DYLD_LIBRARY_PATH"
 else
-    export LD_LIBRARY_PATH="\$TOP/native-libraries-32:\$TOP/native-libraries-64:\$LD_LIBRARY_PATH"
+    export LD_LIBRARY_PATH="\$TOP:\$LD_LIBRARY_PATH"
 fi
 
 exec $VM_NAME \$@
 EOF
 
-chmod +x "$WODEN2_SRC_TOP/woden2.sh"
+chmod +x "$WODEN_SRC_TOP/woden.sh"
