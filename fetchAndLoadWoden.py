da35021fe7168093abf58a9b369ccb2bb6567bf8
Ronie Salgado <roniesalg@gmail.com>
2019-02-16 01:09:20 -0300

    Removing the scripts that are not needed anymore.

--- fetchAndLoadWoden.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-from fetchDependencies import *
-from loadWoden import *
-
-# Satisfy the dependencies
-satisfyDependencies()
-
-# Load the current version of Woden
-loadWodenImage()
--- fetchAndLoadWoden32.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-from fetchDependencies import *
-from loadWoden import *
-
-# Satisfy the dependencies
-satisfy32BitDependencies()
-
-# Load the current version of Woden
-loadWodenImage()
